14887dbae7928a6d7ef95d82a93eb089e64ea7d9
Joel Truher <joel@truher.org>
2026-01-22 13:31:41 -0800

    add recursive tof solver

---
+++ lib/src/test/java/org/team100/lib/targeting/TimeOfFlightRecursionTest.java
@@ -0,0 +1,81 @@
+package org.team100.lib.targeting;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.team100.lib.geometry.GlobalVelocityR2;
+
+import edu.wpi.first.math.geometry.Translation2d;
+
+public class TimeOfFlightRecursionTest {
+    private static final double DELTA = 0.001;
+
+    /** See ShootingMethodTest.testMotionlessParabolic() */
+    @Test
+    void testMotionlessParabolic() {
+        double g = 9.81;
+        Drag d = new Drag(0, 0, 0, 1, 0);
+        double v = 7;
+        InverseRange ir = new InverseRange(d, v, 0);
+        TimeOfFlightRecursion tofr = new TimeOfFlightRecursion(ir, 0.01);
+        Translation2d robotPosition = new Translation2d();
+        GlobalVelocityR2 robotVelocity = GlobalVelocityR2.ZERO;
+        // target is 2m away along +x
+        Translation2d targetPosition = new Translation2d(2, 0);
+        GlobalVelocityR2 targetVelocity = GlobalVelocityR2.ZERO;
+        Optional<TimeOfFlightRecursion.Solution> o = tofr.solve(
+                robotPosition, robotVelocity, targetPosition, targetVelocity);
+        TimeOfFlightRecursion.Solution x = o.orElseThrow();
+        assertEquals(0, x.azimuth().getRadians(), DELTA);
+        assertEquals(0.206, x.elevation().getRadians(), DELTA);
+    }
+
+    /**
+     * See ShootingMethodTest.testAwayFromTarget()
+     * 
+     * This takes a whole lot of iterations to match the shooting method, note the
+     * tight tolerance.
+     */
+    @Test
+    void testAwayFromTarget() {
+        Drag d = new Drag(0.5, 0.025, 0.1, 0.1, 0.1);
+        InverseRange ir = new InverseRange(d, 10, 0);
+        TimeOfFlightRecursion tofr = new TimeOfFlightRecursion(ir, 0.0001);
+        Translation2d robotPosition = new Translation2d();
+        // driving away from the target
+        GlobalVelocityR2 robotVelocity = new GlobalVelocityR2(-2, 0);
+        // target is 2m away along +x
+        Translation2d targetPosition = new Translation2d(2, 0);
+        GlobalVelocityR2 targetVelocity = GlobalVelocityR2.ZERO;
+
+        Optional<TimeOfFlightRecursion.Solution> o = tofr.solve(
+                robotPosition, robotVelocity, targetPosition, targetVelocity);
+        TimeOfFlightRecursion.Solution x = o.orElseThrow();
+        assertEquals(0, x.azimuth().getRadians(), DELTA);
+        assertEquals(0.390, x.elevation().getRadians(), DELTA);
+    }
+
+    /** See ShootingMethodTest.testStrafing() */
+    @Test
+    void testStrafing() {
+        Drag d = new Drag(0.5, 0.025, 0.1, 0.1, 0.1);
+        double v = 7;
+        InverseRange ir = new InverseRange(d, v, 0);
+        TimeOfFlightRecursion tofr = new TimeOfFlightRecursion(ir, 0.001);
+        Translation2d robotPosition = new Translation2d();
+        // driving to the left
+        GlobalVelocityR2 robotVelocity = new GlobalVelocityR2(0, 2);
+        // target is 2m away along +x
+        Translation2d targetPosition = new Translation2d(2, 0);
+        GlobalVelocityR2 targetVelocity = GlobalVelocityR2.ZERO;
+
+        Optional<TimeOfFlightRecursion.Solution> o = tofr.solve(
+                robotPosition, robotVelocity, targetPosition, targetVelocity);
+        TimeOfFlightRecursion.Solution x = o.orElseThrow();
+        assertEquals(-0.484, x.azimuth().getRadians(), DELTA);
+        assertEquals(0.449, x.elevation().getRadians(), DELTA);
+    }
+
+}

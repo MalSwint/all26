b0718d6198a7bf789430c97ab51dbd5cb935af37
Malcolm <824706@seq.org>
2026-02-14 14:30:24 -0800

    Added a "test" auton

---
+++ comp/src/main/java/org/team100/frc2026/auton/AutonPositions.java
@@ -0,0 +1,13 @@
+package org.team100.frc2026.auton;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+/**
+ * Areas that might be useful for autons
+ */
+public class AutonPositions {
+    public static final Pose2d ABOVE_BALL_FIELD = new Pose2d(7.5, 7, Rotation2d.kCW_90deg);
+
+
+}
